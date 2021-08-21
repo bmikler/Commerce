7f2ae837622ff534a6b978750b0c3436982a097a
Bartlomiej Mikler <bartlomiej.mikler@gmail.com>
2021-08-21 19:55:24 +0200

    first commit

---
+++ auctions/admin.py
@@ -0,0 +1,15 @@
+from auctions.models import Article, AuctionList, Category, Comment
+from django.contrib import admin
+
+from .models import *
+
+# Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(AuctionList)
+admin.site.register(Bid)
+admin.site.register(Comment)
+
---
+++ auctions/models.py
@@ -0,0 +1,42 @@
+from typing import ItemsView
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models.deletion import CASCADE, PROTECT
+from django.db.models.fields.related import ForeignKey
+
+class User(AbstractUser):
+    pass
+
+class Category(models.Model):
+    type = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.type
+
+class Article(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=600)
+    image_url = models.URLField()
+    article_category = ForeignKey(Category, on_delete=PROTECT)
+
+    def __str__(self) -> str:
+        return self.title
+
+class AuctionList(models.Model):
+    seller = ForeignKey(User, on_delete=CASCADE)
+    item = ForeignKey(Article, on_delete=CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    end_time = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"{self.id}: {self.item.title} listed by {self.seller}"
+
+class Bid(models.Model):
+    buyer = ForeignKey(User, on_delete=CASCADE)
+    auction = ForeignKey(AuctionList, on_delete=CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Comment(models.Model):
+    author = ForeignKey(User, on_delete=CASCADE)
+    auction_comment = ForeignKey(AuctionList, on_delete=CASCADE)
+    text = models.CharField(max_length=128)
